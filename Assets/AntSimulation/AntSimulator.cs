55ecc9f184cbd69abd8075cdf91486fabfc5f115
Taku <sakisaki200012@gmail.com>
2021-12-27 23:15:56 +0900

    create ant architecture

---
+++ Assets/AntSimulation/AntSimulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AntSimulation
+{
+    public class AntSimulator : MonoBehaviour
+    {
+        private readonly List<Ant> _ants = new List<Ant>();
+
+        /// <summary>
+        /// アリ登録用関数
+        /// </summary>
+        public void Add(Ant ant)
+        {
+            if(!_ants.Contains(ant)) _ants.Add(ant);
+        }
+        
+        private void Update()
+        {
+            foreach (var ant in _ants) ant.Move();
+        }
+    }
+}
