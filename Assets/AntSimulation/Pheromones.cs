dd69cd679775ec9f87d100cada32243af8341a38
Taku <sakisaki200012@gmail.com>
2022-01-08 15:50:19 +0900

    create pheromones

---
+++ Assets/AntSimulation/Pheromones.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace AntSimulation
+{
+    public class Pheromones : MonoBehaviour
+    {
+        // 最大寿命 (s)
+        [SerializeField] private float lifeTimeMax;
+        private float lifeTime;
+
+        // 公開用プロパティ
+        public float LifeTimeMax => lifeTimeMax;
+        public float LifeTime => lifeTime;
+
+        private void Start()
+        {
+            lifeTime = lifeTimeMax;
+        }
+
+
+        private void Update()
+        {
+            // 単純に経過時間を減算
+            lifeTime -= Time.deltaTime;
+
+            if (lifeTime <= 0)
+            {
+                // 寿命が0以下で削除
+                Destroy(this.gameObject);
+            }
+        }
+        
+        /// <summary>
+        /// 残りの寿命を0～1の割合で返す
+        /// </summary>
+        /// <returns></returns>
+        public float CalculateLifeStrength()
+        {
+            var strength = Mathf.Clamp(lifeTime, 0, lifeTimeMax) / lifeTimeMax;
+            return strength;
+        }
+    }
+}
--- Assets/AntSimulation/VimiiAnt.cs
+++ Assets/AntSimulation/VimiiAnt.cs
@@ -1,17 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace AntSimulation
 {
     public class VimiiAnt : Ant
     {
+        [SerializeField] private LayerMask pheromonesLayer;
+
         public override void Find(Transform[] transforms)
         {
-            
+            var pheromonesObj = transforms.Where(x => ((1 << x.gameObject.layer) & pheromonesLayer) != 0);
+            // pheromonesObj.Select(x => x.gameObject.transform.)
         }
 
         public override void Move()
         {
-            
         }
     }
 }
