5255cd16ed4571b07ff377198420cfb03a14ccaa
Yamamoto Yuuya <bf1@outlook.jp>
2022-01-08 18:26:40 +0900

    ちょっと修正

--- Assets/AntSimulation/antAI.cs
+++ Assets/AntSimulation/antAI.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace AntSimulation{
-    public class antdeciding : MonoBehaviour
+    public class antAI : MonoBehaviour
     {
     // Start is called before the first frame update
     // Update is called once per frame
         //進みたい向きのスケール(使わなそう)
         [SerializeField] float speedscale=0.01f;
+        //ランダムに動く範囲のスケール
+        [SerializeField,Range(0.0f,0.5f)] float randomwidth=0.01f;
         //ランダムに動く大きさのスケール
-        [SerializeField] float randomspeedscale=0.01f;
+        [SerializeField] float randomscale=0.01f;
         
         [SerializeField] private TargetSearcher targetSearcher;
 
@@ -30,19 +32,22 @@ namespace AntSimulation{
              Vector3 tmp =new Vector3(0,0,0);
              foreach(var i in transforms){
                  //フェロモンの重心の相対位置tmp
-                 tmp+=i.position-self.position;
+                 tmp+=(i.position-self.position);
                  /*new Vector3(i.position.x-self.position.x,
                  i.position.y-self.position.y,
                  i.position.z-self.position.z);*/
              }
-            tmp+=new Vector3(Random.value*randomspeedscale,Random.value*randomspeedscale,Random.value*randomspeedscale);
+            //tmp+=new Vector3(Random.Range(-1.0f,1.0f)*randomspeedscale,Random.Range(-1.0f,1.0f)*randomspeedscale,Random.Range(-1.0f,1.0f)*randomspeedscale);
+             float n=Random.Range(0.5f-randomwidth,randomwidth+0.5f)*Mathf.PI;
+             tmp+=(Mathf.Cos((n))*self.right+Mathf.Sin(n)*self.forward)*randomscale;
              if (transforms.Length!=0){
                  tmp=(tmp/(transforms.Length+1)).normalized*speedscale;
+                 print(transforms.Length);
             }
             //print(tmp);
             //緑軸方向成分を消去
-            tmp=Vector3.Scale(tmp,(transform.right+transform.forward)).normalized;
-            this.transform.rotation=Quaternion.LookRotation(tmp,Vector3.up);
+            tmp=Vector3.Scale(tmp,(self.right+self.forward)).normalized;
+            this.transform.rotation=Quaternion.LookRotation(tmp,self.up);
         }
     
 
