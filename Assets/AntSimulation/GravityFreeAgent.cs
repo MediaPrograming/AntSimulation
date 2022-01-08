62b81660d2cfde0061994d5a46db723db5953d39
Vimii <vimii8003@gmail.com>
2022-01-08 22:19:50 +0900

    Antsimurater

--- Assets/AntSimulation/GravityFreeAgent.cs
+++ Assets/AntSimulation/GravityFreeAgent.cs
@@ -43,50 +43,50 @@ public class GravityFreeAgent : MonoBehaviour {
 
             return;
         }
-        
-        int rayNum = 4;
-        Ray[] rays = new Ray[rayNum];
-        rays[0] = new Ray(CenterOfBalance.position+transform.forward*rayOffset, transform.forward);
-        rays[1] = new Ray(CenterOfBalance.position+transform.forward*rayOffset, transform.forward - transform.up);
-        rays[2] = new Ray(CenterOfBalance.position+transform.forward*rayOffset, -transform.up);
-        rays[3] = new Ray(CenterOfBalance.position+transform.forward*rayOffset, -transform.forward-transform.up);
-        // rays[4] = new Ray(CenterOfBalance.position+transform.forward*rayOffset, transform.up);
-        
-        RaycastHit[] hitArray = new RaycastHit[rayNum];
-        
-        float minDist = float.PositiveInfinity;
-        int minIndex = -1;
-        for (int i = 0; i < rayNum; i++)
-        {
-            Physics.Raycast(rays[i], out hitArray[i], float.PositiveInfinity);
-            if (hitArray[i].distance > 0 && minDist > hitArray[i].distance)
-            {
-                minDist = hitArray[i].distance;
-                minIndex = i;
-            }
-            Debug.DrawRay(rays[i].origin,rays[i].direction * 100,Color.blue,1,true);
-        }
-        
-        //近くに地形があったら
-        if (minIndex != -1 && minDist < movableDist)
-        {
-            Quaternion q = Quaternion.FromToRotation(
-                transform.up,
-                hitArray[minIndex].normal);
-        
-            transform.rotation *= q;
-            
-            Debug.Log("minDist="+minDist);
-        
-            transform.position = hitArray[minIndex].point + (transform.position - CenterOfBalance.position);
-            
-            Debug.DrawRay(rays[minIndex].origin,rays[minIndex].direction * 100, Color.yellow, 1,true);
-        }
-        else
-        {
-            transform.Rotate(10,10,10);
-            transform.position = transform.position +new Vector3(0,0.01f,0);
-        }
+        //
+        // int rayNum = 4;
+        // Ray[] rays = new Ray[rayNum];
+        // rays[0] = new Ray(CenterOfBalance.position+transform.forward*rayOffset, transform.forward);
+        // rays[1] = new Ray(CenterOfBalance.position+transform.forward*rayOffset, transform.forward - transform.up);
+        // rays[2] = new Ray(CenterOfBalance.position+transform.forward*rayOffset, -transform.up);
+        // rays[3] = new Ray(CenterOfBalance.position+transform.forward*rayOffset, -transform.forward-transform.up);
+        // // rays[4] = new Ray(CenterOfBalance.position+transform.forward*rayOffset, transform.up);
+        //
+        // RaycastHit[] hitArray = new RaycastHit[rayNum];
+        //
+        // float minDist = float.PositiveInfinity;
+        // int minIndex = -1;
+        // for (int i = 0; i < rayNum; i++)
+        // {
+        //     Physics.Raycast(rays[i], out hitArray[i], float.PositiveInfinity);
+        //     if (hitArray[i].distance > 0 && minDist > hitArray[i].distance)
+        //     {
+        //         minDist = hitArray[i].distance;
+        //         minIndex = i;
+        //     }
+        //     Debug.DrawRay(rays[i].origin,rays[i].direction * 100,Color.blue,1,true);
+        // }
+        //
+        // //近くに地形があったら
+        // if (minIndex != -1 && minDist < movableDist)
+        // {
+        //     Quaternion q = Quaternion.FromToRotation(
+        //         transform.up,
+        //         hitArray[minIndex].normal);
+        //
+        //     transform.rotation *= q;
+        //     
+        //     Debug.Log("minDist="+minDist);
+        //
+        //     transform.position = hitArray[minIndex].point + (transform.position - CenterOfBalance.position);
+        //     
+        //     Debug.DrawRay(rays[minIndex].origin,rays[minIndex].direction * 100, Color.yellow, 1,true);
+        // }
+        // else
+        // {
+        //     transform.Rotate(10,10,10);
+        //     transform.position = transform.position +new Vector3(0,0.01f,0);
+        // }
 
     }
 
